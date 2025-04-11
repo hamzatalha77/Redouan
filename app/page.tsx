d54f5adf687f6520c38e6e952d6fba82148a6bf2
hamzatalha77 <hamza_ta7@hotmail.com>
2025-04-11 12:07:55 +0100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello redouuan!</h1>
+  return <h1 className="text-3xl font-bold underline">Hello redouvan!</h1>
 }
