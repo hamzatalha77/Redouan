921cdac20c8a13101dc57dcb6916b8a360c2df59
hamzatalha77 <hamza_ta7@hotmail.com>
2025-05-30 21:24:32 +0100

    commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import { DragCards } from '@/app/components/drag'
 import Example from '@/app/components/horizontalScrollCarousel'
 import React from 'react'
 import { Exampleimages } from '../components/mouseImageTrail'
-import TrippyScroll from '../components/trippyScroll'
+// import TrippyScroll from '../components/trippyScroll'
 import { DrawCircleText } from '../components/drawCircleText'
 
 const About = () => {
@@ -12,7 +12,7 @@ const About = () => {
       <Example />
       <Exampleimages />
       <DrawCircleText />
-      <TrippyScroll />
+      {/* <TrippyScroll /> */}
       <DragCards />
     </div>
   )
