a5683993f51333e58d6f549dcac0385d47790ca0
hamzatalha77 <hamza_ta7@hotmail.com>
2025-06-15 13:19:01 +0100

    commit

--- app/components/textParallaxContent.tsx
+++ app/components/textParallaxContent.tsx
@@ -111,7 +111,7 @@ const ExampleContent = () => (
       </p>
       <p className="mb-8 text-xl text-neutral-600 md:text-2xl">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        reiciendis blanditiis aliquam aut fugit si
+        reiciendis blanditiis aliquam aut fugit
       </p>
       <div className="bg-white min-h-[200px]  items-center justify-center">
         <button className="px-6 py-2 font-medium bg-white dark:bg-black text-black dark:text-white w-fit transition-all shadow-[3px_3px_0px_gray] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
