c2b8483dfd21e6f0502e8db666cd040ad78352ea
hamzatalha77 <hamza_ta7@hotmail.com>
2025-06-22 11:52:51 +0100

    commit

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -53,7 +53,6 @@ const Article = () => {
                             <p className="text-sm text-gray-400 dark:text-neutral-400">
                               Leyla is a Customer Success Specialist at Preline
                               and spends her time speaking to in-house
-                              recruiters
                             </p>
                           </div>
 
