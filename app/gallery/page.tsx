7c98df11e282390442e774cbcc237f559936dde0
hamzatalha77 <hamza_ta7@hotmail.com>
2025-07-12 14:55:18 +0100

    Switches wrapper to React.Fragment for improved structure
    Replaces the `<div>` wrapper with `<React.Fragment>` in the Gallery component to enhance semantic structure and reduce unnecessary DOM elements. Also adjusts import order for better organization and readability.

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,20 +1,20 @@
 'use client'
+import React from 'react'
 import { DragCards } from '@/app/components/drag'
 import Example from '@/app/components/horizontalScrollCarousel'
-import React from 'react'
 import { Exampleimages } from '../components/mouseImageTrail'
 import { DrawCircleText } from '../components/drawCircleText'
 import FeaturedWork from '../components/FeaturedWork'
 
 const Gallery = () => {
   return (
-    <div>
+    <React.Fragment>
       <FeaturedWork />
       <Example />
       <Exampleimages />
       <DrawCircleText />
       <DragCards />
-    </div>
+    </React.Fragment>
   )
 }
 
